0df5bc7fd43a31f773f7e0a05745e3701ff64340
code-reaper08 <v.i.s.h.w.a.08.11.2001@gmail.com>
2021-07-15 22:59:59 +0530

    Successfully added fetching mechanism for fetching data from gist

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 const para1 = document.getElementById('para1');
 
 
-fet
+async function getdata() {
+    const response = await fetch("https://gist.githubusercontent.com/code-reaper08/bb92db261148dfed81ba4d07c964a004/raw/af26f7e35dddaa56987118963b44362eb1e90074/TA.json");
+    const data = await response.json();
+    console.log(data)
+}
+getdata();
