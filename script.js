05f6470253a789cb5fecff6a011df769ac39e186
code-reaper08 <v.i.s.h.w.a.08.11.2001@gmail.com>
2021-07-15 23:01:59 +0530

    Optimized fetching

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const para1 = document.getElementById('para1');
 async function getdata() {
     const response = await fetch("https://gist.githubusercontent.com/code-reaper08/bb92db261148dfed81ba4d07c964a004/raw/af26f7e35dddaa56987118963b44362eb1e90074/TA.json");
     const data = await response.json();
-    console.log(data)
+    // console.log(data)
+    intro = data.intro;
+    para1.innerText= intro;
 }
 getdata();
