c4b466a7ca9a6b932e2f05efb35500f92aa7486b
code-reaper08 <v.i.s.h.w.a.08.11.2001@gmail.com>
2021-07-17 20:23:12 +0530

    Need to fix toggler #3

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
 const para1 = document.getElementById('para1');
 const ticon = document.querySelector('#trans > img');
 
-let clickcounter = 0;
-ticon.onclick = function counter() { 
-    clickcounter++;
-    if (clickcounter == 2) {
-        getdataTA()
-    }
-    else if (clickcounter == 1){
-        getdataEN()
-    }
-    else if (clickcounter > 2){
-        continue
-        counter()
-    }
+// let clickcounter = 0;
+// ticon.onclick = function counter() { 
+//     clickcounter++;
+//     if (clickcounter == 2) {
+//         getdataTA()
+//     }
+//     else if (clickcounter == 1){
+//         getdataEN()
+//     }
+//     else if (clickcounter > 2){
+//         continue
+//         counter()
+//     }
+//  }
+function translate() { 
+    $('#toggle-event').on()
  }
-
 async function getdataTA() {
     const response = await fetch("https://gist.githubusercontent.com/code-reaper08/bb92db261148dfed81ba4d07c964a004/raw/af26f7e35dddaa56987118963b44362eb1e90074/TA.json");
     const data = await response.json();
